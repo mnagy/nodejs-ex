175bcd2923e46110a77fcf02009763d2d2403209
sspeiche <sspeiche@redhat.com>
2015-06-17 16:05:32 -0400

    WIP: Move towards a more common welcome page and use Express+Jade

--- server.js
+++ server.js
@@ -1,54 +1,29 @@
-var util = require('util');
-var url = require('url');
-var qs = require('querystring');
-var os = require('os')
-var port = process.env.PORT || process.env.port || process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var ip = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-var nodeEnv = process.env.NODE_ENV || 'unknown';
-
+#!/bin/env node
+//  OpenShift sample Node application
 var express = require('express');
-var app = express();
+var fs      = require('fs');
+var jade    = require('jade');
+var app     = express();
+app.set('views', './views');
+app.set('view engine', 'jade');
 
-app.use(function(req, res, next) {
-	var url_parts = url.parse(req.url, true);
+app.use(express.static(__dirname + '/public'))
 
-	var body = '';
-	req.on('data', function (data) {
-		body += data;
-	});
-	req.on('end', function () {
-		var formattedBody = qs.parse(body);
+var port = process.env.PORT || process.env.port || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-		res.writeHead(200, {'Content-Type': 'text/plain'});
+// Forward on to jade
+app.get('/', function (req, res) {
+  res.render('index', { title : 'Node.js simple example for OpenShift' });
+})
 
-		res.write('Hello OpenShift World! This is a Node.js-based sample application.\n');
-		res.write('Host: ' + req.headers.host + '\n');
-		res.write('\n');
-		res.write('node.js Production Mode: ' + (nodeEnv == 'production' ? 'yes' : 'no') + '\n');
-		res.write('\n');
-		res.write('HTTP/' + req.httpVersion +'\n');
-		res.write('Request headers:\n');
-		res.write(util.inspect(req.headers, null) + '\n');
-		res.write('Request query:\n');
-		res.write(util.inspect(url_parts.query, null) + '\n');
-		res.write('Request body:\n');
-		res.write(util.inspect(formattedBody, null) + '\n');
-		res.write('\n');
-		res.write('Host: ' + os.hostname() + '\n');
-		res.write('OS Type: ' + os.type() + '\n');
-		res.write('OS Platform: ' + os.platform() + '\n');
-		res.write('OS Arch: ' + os.arch() + '\n');
-		res.write('OS Release: ' + os.release() + '\n');
-		res.write('OS Uptime: ' + os.uptime() + '\n');
-		res.write('OS Free memory: ' + os.freemem() / 1024 / 1024 + 'mb\n');
-		res.write('OS Total memory: ' + os.totalmem() / 1024 / 1024 + 'mb\n');
-		res.write('OS CPU count: ' + os.cpus().length + '\n');
-		res.write('OS CPU model: ' + os.cpus()[0].model + '\n');
-		res.write('OS CPU speed: ' + os.cpus()[0].speed + 'mhz\n');
-		res.end('\n');
-	});
-	next();
+// error handling
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500).body('Something bad happened!');
 });
 
 app.listen(port, ip);
 console.log('Server running on ' + ip + ':' + port);
+
+
