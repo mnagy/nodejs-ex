c7f2b44ee7d0ff73f4bb670660b5f688eadcddbe
Martin Nagy <nagy.martin@gmail.com>
2015-06-17 21:55:59 +0200

    Add mongodb code to server.js

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ var os = require('os')
 var port = process.env.PORT || process.env.port || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var nodeEnv = process.env.NODE_ENV || 'unknown';
+// TODO: Handle missing database service name
+var mongoIp = process.env.DATABASE_SERVICE_NAME;
 
 var express = require('express');
-var app = express();
+var mongo = require('mongodb').MongoClient;
 
+var app = express();
 app.use(function(req, res, next) {
 	var url_parts = url.parse(req.url, true);
 
@@ -22,6 +25,7 @@ app.use(function(req, res, next) {
 		res.writeHead(200, {'Content-Type': 'text/plain'});
 
 		res.write('Hello OpenShift World! This is a Node.js-based sample application.\n');
+		res.write('Mongo IP: ' + mongoIp + '\n');
 		res.write('Host: ' + req.headers.host + '\n');
 		res.write('\n');
 		res.write('node.js Production Mode: ' + (nodeEnv == 'production' ? 'yes' : 'no') + '\n');
