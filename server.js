616fba30ca3b17d75b6ef6f68df9aeef7750b00e
Martin Nagy <nagy.martin@gmail.com>
2015-06-19 12:19:25 +0200

    Fix error handling

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.get('/pagecount', function (req, res) {
 // error handling
 app.use(function(err, req, res, next){
 	console.error(err.stack);
-	res.status(500).body('Something bad happened!');
+	res.status(500).send('Something bad happened!');
 });
 
 initDb(function(err){
