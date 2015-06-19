fb9a44b1ffb795e7a8a969f83007207ba74cb9a4
sspeiche <sspeiche@redhat.com>
2015-06-19 09:35:23 -0400

    Cleaned up tabs vs spaces

--- server.js
+++ server.js
@@ -11,76 +11,76 @@ var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL;
 var mongoURLLabel = "";
 if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-	var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
-	var mongoHost = process.env[mongoServiceName + "_SERVICE_HOST"];
-	var mongoPort = process.env[mongoServiceName + "_SERVICE_PORT"];
-	var mongoUser = process.env.MONGODB_USER
-	if (mongoHost && mongoPort && process.env.MONGODB_DATABASE) {
-		mongoURLLabel = mongoURL = 'mongodb://';
-		if (process.env.MONGODB_USER && process.env.MONGODB_PASSWORD) {
-			mongoURL += process.env.MONGODB_USER + ':' + process.env.MONGODB_PASSWORD + '@';
-		}
-		// Provide UI label that excludes user id and pw
+  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
+  var mongoHost = process.env[mongoServiceName + "_SERVICE_HOST"];
+  var mongoPort = process.env[mongoServiceName + "_SERVICE_PORT"];
+  var mongoUser = process.env.MONGODB_USER
+  if (mongoHost && mongoPort && process.env.MONGODB_DATABASE) {
+    mongoURLLabel = mongoURL = 'mongodb://';
+    if (process.env.MONGODB_USER && process.env.MONGODB_PASSWORD) {
+      mongoURL += process.env.MONGODB_USER + ':' + process.env.MONGODB_PASSWORD + '@';
+    }
+    // Provide UI label that excludes user id and pw
 
-		mongoURLLabel += mongoHost + ':' + mongoPort + '/' + process.env.MONGODB_DATABASE;
-		mongoURL += mongoHost + ':' + mongoPort + '/' + process.env.MONGODB_DATABASE;
-	}
+    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + process.env.MONGODB_DATABASE;
+    mongoURL += mongoHost + ':' + mongoPort + '/' + process.env.MONGODB_DATABASE;
+  }
 }
 var db = null;
 var dbDetails = new Object();
 
 var initDb = function(callback) {
-	if (mongoURL == null) return;
+  if (mongoURL == null) return;
 
-	var mongodb = require('mongodb');	
-	if (mongodb == null) return;
-	
-	mongodb.connect(mongoURL, function(err, conn) {
-		if (err) {
-			callback(err);
-			return;
-		}
+  var mongodb = require('mongodb');  
+  if (mongodb == null) return;
 
-		db = conn;
-		dbDetails.databaseName = db.databaseName;
-		dbDetails.url = mongoURLLabel;
-		dbDetails.type = 'MongoDB';
+  mongodb.connect(mongoURL, function(err, conn) {
+    if (err) {
+      callback(err);
+      return;
+    }
 
-		console.log("Connected to MongoDB at: " + mongoURL);
-	});
+    db = conn;
+    dbDetails.databaseName = db.databaseName;
+    dbDetails.url = mongoURLLabel;
+    dbDetails.type = 'MongoDB';
+
+    console.log("Connected to MongoDB at: " + mongoURL);
+  });
 };
 
 app.get('/', function (req, res) {
-	if (db) {
-		var col = db.collection('counts');
-		// Create a document with request IP and current time of request
-		col.insert({ip: req.ip, date: Date.now()});
-		col.count(function(err, count){
-			res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-		});
-	} else { 
-		res.render('index.html', { pageCountMessage : "No DB configured"});
-	}
+  if (db) {
+    var col = db.collection('counts');
+    // Create a document with request IP and current time of request
+    col.insert({ip: req.ip, date: Date.now()});
+    col.count(function(err, count){
+      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+    });
+  } else {
+    res.render('index.html', { pageCountMessage : "No DB configured"});
+  }
 });
 
 app.get('/pagecount', function (req, res) {
-	if (db) {
-		db.collection('counts').count(function(err, count ){
-			res.send('{ pageCount: ' + count +'}');
-		});
-	} else { 
-		res.send('{ pageCount: -1 }');
-	}
+  if (db) {
+    db.collection('counts').count(function(err, count ){
+      res.send('{ pageCount: ' + count +'}');
+    });
+  } else { 
+    res.send('{ pageCount: -1 }');
+  }
 });
 
 // error handling
 app.use(function(err, req, res, next){
-	console.error(err.stack);
-	res.status(500).body('Something bad happened!');
+  console.error(err.stack);
+  res.status(500).body('Something bad happened!');
 });
 
 initDb(function(err){
-	console.log('Error connecting to Mongo. Message:\n'+err);
+  console.log('Error connecting to Mongo. Message:\n'+err);
 });
 
 app.listen(port, ip);
