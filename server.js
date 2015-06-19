af5cbb573a2770f7503b50a72ee265ae33e369c1
sspeiche <sspeiche@redhat.com>
2015-06-19 09:10:57 -0400

    Clean up some spacing and don't show id/pw

--- server.js
+++ server.js
@@ -9,17 +9,21 @@ app.engine('html', require('ejs').renderFile);
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL;
+var mongoURLLabel = "";
 if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
 	var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
 	var mongoHost = process.env[mongoServiceName + "_SERVICE_HOST"];
 	var mongoPort = process.env[mongoServiceName + "_SERVICE_PORT"];
 	var mongoUser = process.env.MONGODB_USER
 	if (mongoHost && mongoPort && process.env.MONGODB_DATABASE) {
-      mongoURL = 'mongodb://';
-      if (process.env.MONGODB_USER && process.env.MONGODB_PASSWORD) {
-        mongoURL += process.env.MONGODB_USER + ':' + process.env.MONGODB_PASSWORD + '@';
-	  }
-      mongoURL += mongoHost + ':' + mongoPort + '/' + process.env.MONGODB_DATABASE;
+		mongoURLLabel = mongoURL = 'mongodb://';
+		if (process.env.MONGODB_USER && process.env.MONGODB_PASSWORD) {
+			mongoURL += process.env.MONGODB_USER + ':' + process.env.MONGODB_PASSWORD + '@';
+		}
+		// Provide UI label that excludes user id and pw
+
+		mongoURLLabel += mongoHost + ':' + mongoPort + '/' + process.env.MONGODB_DATABASE;
+		mongoURL += mongoHost + ':' + mongoPort + '/' + process.env.MONGODB_DATABASE;
 	}
 }
 var db = null;
@@ -39,34 +43,34 @@ var initDb = function(callback) {
 
 		db = conn;
 		dbDetails.databaseName = db.databaseName;
-		dbDetails.url = db.options.url;
-	  	dbDetails.type = 'MongoDB';
+		dbDetails.url = mongoURLLabel;
+		dbDetails.type = 'MongoDB';
 
 		console.log("Connected to MongoDB at: " + mongoURL);
 	});
 };
 
 app.get('/', function (req, res) {
-  if (db) {
-	  var col = db.collection('counts');
-	  // Create a document with request IP and current time of request
-	  col.insert({ip: req.ip, date: Date.now()});
-	  col.count(function(err, count){
- 		res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-	  });
-  } else { 
-    res.render('index.html', { pageCountMessage : "No DB configured"});
-  }
+	if (db) {
+		var col = db.collection('counts');
+		// Create a document with request IP and current time of request
+		col.insert({ip: req.ip, date: Date.now()});
+		col.count(function(err, count){
+			res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+		});
+	} else { 
+		res.render('index.html', { pageCountMessage : "No DB configured"});
+	}
 });
 
 app.get('/pagecount', function (req, res) {
-  if (db) {
-	  db.collection('counts').count(function(err, count ){
-		res.send('{ pageCount: ' + count +'}');
-	  });
-  } else { 
-	  res.send('{ pageCount: -1 }');
-  }
+	if (db) {
+		db.collection('counts').count(function(err, count ){
+			res.send('{ pageCount: ' + count +'}');
+		});
+	} else { 
+		res.send('{ pageCount: -1 }');
+	}
 });
 
 // error handling
@@ -81,5 +85,3 @@ initDb(function(err){
 
 app.listen(port, ip);
 console.log('Server running on ' + ip + ':' + port);
-
-
